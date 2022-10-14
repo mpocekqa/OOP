dcadd0a9c25abd1241ba3933f62996089ea9c742
mpocekqa <milovanpocek@gmail.com>
2022-10-14 13:28:59 +0200

    prvi commit

---
+++ src/Variables/declarationAndInitialization.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class declarationAndInitialization {
+    public static void main(String []args) {
+        //for numeric values, we use type integer
+        int number = 2;
+        String ime = "Milovan";
+        System.out.println(ime);
+    }
+}
